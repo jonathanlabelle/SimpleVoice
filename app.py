018ca28915611c10b4c93e3ba4feafd0d6425758
Jonathan <labelle.jonathan1@gmail.com>
2022-06-13 08:05:41 -0400

    login/user done, create update users

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from forms import LogInForm, SignupForm
+from forms import LogInForm, SignupForm, CreateClientForm, EditClientGetIDForm, EditClientInformationForm
 
-from model import app, db, create_db, Users
+from model import app, db, create_db, Users, Clients
 
 engine = sqlalchemy.create_engine('mysql://root:root@localhost/SimpleVoice')
 login_manager = LoginManager()
@@ -29,6 +29,7 @@ def login():
             login_user(user)
             return redirect(url_for('home'))
         flash('invalid username/password combination')
+        return redirect(url_for('login'))
     return render_template('login.html', form=form)
 
 
@@ -64,6 +65,69 @@ def home():
     return render_template('home.html', user=user)
 
 
-if __name__ == '__main__':
-    create_db()
+@app.route('/create_client', methods=['POST', 'GET'])
+@login_required
+def create_client():
+    form = CreateClientForm()
+    if form.validate_on_submit():
+        client = Clients(name=form.clientName.data, email=form.clientEmail.data,
+                         phone=form.clientPhone.data, reference=form.clientReference.data,
+                         user=current_user.id)
+        session.add(client)
+        session.commit()
+        return redirect(url_for('home'))
+    return render_template('create_client.html', form=form)
 
+
+@app.route('/edit_client_get_id', methods=['POST', 'GET'])
+@login_required
+def edit_client_get_id():
+    form = EditClientGetIDForm()
+    if form.validate_on_submit():
+        client = Clients.query.filter_by(client_id=form.clientID.data).first()
+        if client.user == current_user.id:
+            return redirect(url_for('edit_client_information', client_id=form.clientID.data))
+        else:
+            flash("Client {} doesn't exists".format(form.clientID.data))
+    return render_template('edit_client.html', form=form)
+
+
+@app.route('/edit_client_information', methods=['POST', 'GET'])
+@login_required
+def edit_client_information():
+    client_id = int(request.args.get('client_id'))
+    client = Clients.query.filter_by(client_id=client_id).first()
+    form = EditClientInformationForm()
+    if request.method == 'GET':
+        return render_template('edit_client_information.html', client=client, client_id=client_id, form=form)
+    if form.validate_on_submit():
+        return redirect(url_for('home'))
+
+
+@app.route('/edit_client_information_update/<client_id>', methods=['POST'])
+@login_required
+def edit_client_information_update(client_id):
+        client = Clients.query.filter_by(client_id=client_id).first()
+        form = EditClientInformationForm()
+        if form.clientName.data:
+            client.name = form.clientName.data
+        if form.clientEmail.data:
+            client.email = form.clientEmail.data
+        if form.clientPhone.data:
+            client.phone = form.clientPhone.data
+        if form.clientReference.data:
+            client.reference = form.clientReference.data
+        db.session.commit()
+        return redirect(url_for('home'))
+
+
+@app.route('/test', methods=['POST'])
+@login_required
+def test():
+    form = EditClientInformationForm()
+    name = form.clientName.data
+    return render_template('test.html', name=name)
+
+
+if __name__ == '__main__':
+    print('allo')
--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, validators, EmailField, IntegerField
+from wtforms.validators import InputRequired, DataRequired, Length, EqualTo, Optional
 
 
 class SignupForm(FlaskForm):
     companyName = StringField('companyName', validators=[DataRequired()])
     loginName = StringField('loginName', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, message ='Password must be at least'
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, message='Password must be at least'
                                                                                             ' 5 characters')])
     confirm = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password',
                                                                                     message='Passwords do not match')])
@@ -17,3 +17,24 @@ class LogInForm(FlaskForm):
     loginName = StringField('Login Name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log in')
+
+
+class CreateClientForm(FlaskForm):
+    clientName = StringField('Client Name', validators=[DataRequired()])
+    clientEmail = EmailField('Client e-mail', validators=[DataRequired(), validators.Email()])
+    clientPhone = StringField('Client phone number',validators=[DataRequired(), Length(min=10, max=10)])
+    clientReference = StringField('Client reference person', validators=[DataRequired()])
+    submit = SubmitField('Create account')
+
+
+class EditClientGetIDForm(FlaskForm):
+    clientID = IntegerField('Client ID', validators=[DataRequired()])
+    submit = SubmitField('Edit client')
+
+
+class EditClientInformationForm(FlaskForm):
+    clientName = StringField('Client Name', validators=[Optional()])
+    clientEmail = EmailField('Client e-mail', validators=[validators.Email(), Optional()])
+    clientPhone = StringField('Client phone number', validators=[Length(min=10, max=10), Optional()])
+    clientReference = StringField('Client reference person', validators=[Optional()])
+    submit = SubmitField('Edit client')
